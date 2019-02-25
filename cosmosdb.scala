a24aed9c3e24775d0ee956bc14448465ee7bce15
Koji Shimizu <koji.shimizu@outlook.com>
2019-02-25 19:09:41 +0900

    Update cosmosdb.scala

--- cosmosdb.scala
+++ cosmosdb.scala
@@ -1,13 +1,17 @@
+/*
+ *  Note: The following code works with Databricks Runtime Version 5.2. 
+ *        The error will be thrown with 5.1, and saying "error: object cosmosdb is not a member of package com.microsoft.azure".
+ */
+
 // Databricks notebook source
-// Import Necessary Libraries
 import com.microsoft.azure.cosmosdb.spark.schema._
 import com.microsoft.azure.cosmosdb.spark._
 import com.microsoft.azure.cosmosdb.spark.config.Config
 
 // Configure connection to your collection
 val readConfig = Map(
-  "Endpoint" -> "https://kojis.documents.azure.com:443/",
-  "Masterkey" -> "tLaMHODLLfp2b9mcWLQ0rGJgxwkUUd79Q4LW9X9nSdv5l0i4oekVl35pgoY1bpvLElQWed93iLgVMIDY6jjewQ==",
+  "Endpoint" -> "https://xxxxxxxx.documents.azure.com:443/",
+  "Masterkey" -> "taMHODLLfp2b9mcWLQ0rGJgxwkUUd79Q4LW9X9nSdv5l0i4oekVl35pgoY1bpvLElQWed93iLgVMIDY6jjewQ==",
   "Database" -> "iotdata",
   "PreferredRegions" -> "Japan East;Japan West;",
   "Collection" -> "iot"
